62368e32e7241be0757af9e934c411529e89657f
Tosipaolo <tosipa99@outlook.it>
2023-05-18 12:35:51 +0200

    Added AWGN script

---
+++ AWGN.m
@@ -0,0 +1,65 @@
+clear;
+close all;
+
+% Range EB/N0
+EbNo = 1:12;
+snr = EbNo(3)
+
+%Rate range
+%R = [1/4, ...];
+r = 5/6;
+
+% LDPC configurations
+
+ParityMatrix = dvbs2ldpc(r);
+
+cfg_E = ldpcEncoderConfig(ParityMatrix)
+cfg_D = ldpcDecoderConfig(cfg_E);
+
+
+% Iteration configurations
+
+Numbits = 1e6;
+NumBlocks = ceil(Numbits/cfg_E.NumInformationBits);
+Numbits = cfg_E.NumInformationBits*NumBlocks;
+
+bitstosend = randi([0 1],cfg_E.NumInformationBits,NumBlocks);
+
+%bitstosend = ones(Numbits,1);
+max_iterations = 50;
+
+% Errors per iteration (?)
+
+%Modulation Order QAM
+    % 2 -> BPSK
+M = 2;
+
+%Block Lenght
+
+%Infobits
+
+% Encoder LDPC
+encoded_bits = ldpcEncode(bitstosend,cfg_E);
+
+% QAM Mod
+QAM_mod = qammod(encoded_bits,M,'InputType','bit','PlotConstellation', false);
+
+% AWGN Channel
+NoisySignal = awgn(QAM_mod,snr);
+
+% QAM Demod
+QAM_demod = qamdemod(NoisySignal,M,'OutputType','llr');
+
+% Decoder LDPC
+decoded_bits = ldpcDecode(QAM_demod,cfg_D,max_iterations,"DecisionType","soft");
+
+%% Performance eval
+
+Numerrors = sum(bitstosend ~= (decoded_bits < 0),"all");
+
+BER = Numerrors/Numbits
+
+
+
+
+
